37391344b2f03493439f1402ff374908ecd17ebd
MarcJacob <marcjacobpro@gmail.com>
2025-07-30 17:36:19 +0300

    Added primitive GDI support, clearing to blue.

--- Sources/SynergyMain.cpp
+++ Sources/SynergyMain.cpp
@@ -10,17 +10,41 @@
 #define CLIENT_MODULE_FILENAME ".\\SynergyClientLib"
 #define WCLIENT_MODULE_FILENAME L".\\SynergyClientLib"
 
+union PixelRGBA
+{
+	struct
+	{
+		uint8_t a, r, g, b;
+	};
+
+	uint32_t full;
+};
+
+// Global context state for the Win32 application layer.
 struct Win32AppContext
 {
 	HINSTANCE ProgramInstance;
-	HWND MainWindow;
-
 	bool bRunning = false;
 
 	bool bUsingConsole = true;
 
 	// Synergy Client dll module
 	HMODULE ClientModule;
+
+	// Window & Bitmap data
+	HWND MainWindow;
+	HDC MainWindowDC;
+	HBITMAP MainWindowBitmap;
+	HDC MainWindowBitmapDC;
+
+	// Render Pixel data
+	PixelRGBA* PixelBuffer;
+	size_t PixelBufferWidth;
+	size_t PixelBufferHeight;
+
+	// Memory arena for dynamic memory allocations in the Client app.
+	uint8_t* ClientAppMemory;
+	size_t ClientAppMemorySize;
 };
 
 struct SynergyClientAPI
@@ -79,10 +103,56 @@ void UnloadClientModule()
 
 LRESULT CALLBACK MainWindowProc(HWND window, UINT messageType, WPARAM wParam, LPARAM lParam)
 {
+	BITMAPINFO bitmapInfo = {};
+
 	switch (messageType)
 	{
 	case(WM_CLOSE):
 		Win32App.bRunning = false;
+		break;
+	case(WM_SIZE):
+
+		// Do not do anything if Window got minimized.
+		if (wParam == SIZE_MINIMIZED)
+		{
+			break;
+		}
+
+		// Re allocate render bitmap if size actually changed.
+		int16_t newWidth, newHeight;
+		newWidth = LOWORD(lParam);
+		newHeight = HIWORD(lParam);
+
+		if (Win32App.PixelBuffer != nullptr 
+			&& Win32App.PixelBufferWidth == newWidth
+			&& Win32App.PixelBufferHeight == newHeight)
+		{
+			break;
+		}
+
+		Win32App.PixelBufferWidth = newWidth;
+		Win32App.PixelBufferHeight = newHeight;
+		Win32App.PixelBuffer = nullptr;
+			
+		bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo);
+		bitmapInfo.bmiHeader.biWidth = newWidth;
+		bitmapInfo.bmiHeader.biHeight = -newHeight; // Let's stick to upper-left origin.
+		bitmapInfo.bmiHeader.biPlanes = 1;
+		bitmapInfo.bmiHeader.biBitCount = 32;
+		bitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+		Win32App.MainWindowBitmap = CreateDIBSection(Win32App.MainWindowDC, &bitmapInfo,
+			DIB_RGB_COLORS, reinterpret_cast<void**>(&Win32App.PixelBuffer), NULL, NULL);
+
+		if (Win32App.MainWindowBitmap == 0 || Win32App.PixelBuffer == nullptr)
+		{
+			std::cerr << "ERROR: Failed to allocate bitmap if size " << newWidth << " x " << newHeight << " !\n";
+			break;
+		}
+
+		Win32App.MainWindowBitmapDC = CreateCompatibleDC(Win32App.MainWindowDC);
+		SelectObject(Win32App.MainWindowBitmapDC, Win32App.MainWindowBitmap);
+
 		break;
 	case(WM_KEYDOWN):
 		std::cout << "Reloading Client Module.\n";
@@ -119,6 +189,8 @@ void CreateMainWindow()
 		std::cerr << "Failed to create main window. Error Code = " << GetLastError() << "\n";
 		return;
 	}
+
+	Win32App.MainWindowDC = GetDC(Win32App.MainWindow);
 
 	ShowWindow(Win32App.MainWindow, 1);
 }
@@ -177,11 +249,26 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR pCmdLine, int
 		ClientAPI.Hello();
 	}
 
+	// Message processing & Drawing loop.
 	MSG message;
-	while (Win32App.bRunning && GetMessage(&message, Win32App.MainWindow, NULL, NULL))
+	while (Win32App.bRunning )
 	{
-		TranslateMessage(&message);
-		DispatchMessage(&message);
+		while (PeekMessage(&message, Win32App.MainWindow, NULL, NULL, PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		
+		// Clear screen to blue.
+		for (int x = 0; x < Win32App.PixelBufferWidth; x++)
+		{
+			for (int y = 0; y < Win32App.PixelBufferHeight; y++)
+			{
+				Win32App.PixelBuffer[x * Win32App.PixelBufferHeight + y].full = 0xff0000ff;
+			}
+		}
+		
+		BitBlt(Win32App.MainWindowDC, 0, 0, Win32App.PixelBufferWidth, Win32App.PixelBufferHeight, Win32App.MainWindowBitmapDC, 0, 0, SRCCOPY);
 	}
 
 	CleanupMainWindow();
