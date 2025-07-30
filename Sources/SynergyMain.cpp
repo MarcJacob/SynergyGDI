8af265193fb754dce01f440ebcc0b61ba056e78e
MarcJacob <marcjacobpro@gmail.com>
2025-07-30 17:36:18 +0300

    Simple window opening and closing code with console alloc.

--- Sources/SynergyMain.cpp
+++ Sources/SynergyMain.cpp
@@ -5,30 +5,128 @@
 
 #include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR pCmdLine, int nCmdShow)
+#define CLIENT_MODULE_FILENAME ".\\SynergyClientLib"
+#define WCLIENT_MODULE_FILENAME L".\\SynergyClientLib"
+
+struct Win32AppContext
+{
+	HINSTANCE ProgramInstance;
+	HWND MainWindow;
+
+	bool bRunning = false;
+
+	bool bUsingConsole = true;
+
+	// Synergy Client dll module
+	HMODULE ClientModule;
+};
+
+static Win32AppContext Win32App;
+
+void LoadClientModule()
+{
+	Win32App.ClientModule = LoadLibrary(WCLIENT_MODULE_FILENAME);
+	if (Win32App.ClientModule == nullptr)
+	{
+		std::cerr << "Error: Couldn't load Client Library. Make sure \"" << CLIENT_MODULE_FILENAME << "\" exists.\n";
+		return;
+	}
+}
+
+void UnloadClientModule()
+{
+	FreeLibrary(Win32App.ClientModule);
+}
+
+LRESULT CALLBACK MainWindowProc(HWND window, UINT messageType, WPARAM wParam, LPARAM lParam)
+{
+	switch (messageType)
+	{
+	case(WM_CLOSE):
+		Win32App.bRunning = false;
+		break;
+	default:
+		break;
+	}
+	return DefWindowProc(window, messageType, wParam, lParam);
+}
+
+void CreateMainWindow()
+{
+	static const LPCWSTR MAIN_WINDOW_CLASS_NAME = L"Synergy Main Window Class";
+
+	// Register Main Window Class
+	{
+		WNDCLASS mainWindowClass = {};
+
+		mainWindowClass.hInstance = Win32App.ProgramInstance;
+		mainWindowClass.lpszClassName = MAIN_WINDOW_CLASS_NAME;
+		mainWindowClass.lpfnWndProc = MainWindowProc;
+		mainWindowClass.style = CS_OWNDC;
+
+		RegisterClass(&mainWindowClass);
+	}
+	
+	Win32App.MainWindow = CreateWindow(MAIN_WINDOW_CLASS_NAME, L"Synergy", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		NULL, NULL, Win32App.ProgramInstance, NULL);
+
+	if (Win32App.MainWindow == NULL)
+	{
+		std::cerr << "Failed to create main window. Error Code = " << GetLastError() << "\n";
+		return;
+	}
+
+	ShowWindow(Win32App.MainWindow, 1);
+}
+
+void CleanupMainWindow()
+{
+
+}
+
+void CreateConsole()
 {
 	AllocConsole();
-
+	
 	// Redirect standard out to allocated console.
 	FILE* fDummy;
 	freopen_s(&fDummy, "CONOUT$", "w", stdout);
 
-	// TEST Hot reload the client library on a loop
-	while (1)
+	// Redirect error out to allocated console.
+	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+}
+
+void CloseConsole()
+{
+	FreeConsole();
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR pCmdLine, int nCmdShow)
+{
+	if (Win32App.bUsingConsole)
 	{
-		decltype(OutputHelloWorldFromCore)* Func;
-		HMODULE ClientLibModule = LoadLibrary(L"SynergyClientLib.dll");
-		if (ClientLibModule == nullptr)
-		{
-			return 1;
-		}
+		CreateConsole();
+	}
 
-		Func = reinterpret_cast<decltype(Func)>(GetProcAddress(ClientLibModule, "OutputHelloWorldFromCore"));
-		Func();
+	LoadClientModule();
 
-		FreeLibrary(ClientLibModule);
+	CreateMainWindow();
 
-		system("pause");
+	if (Win32App.MainWindow != nullptr && Win32App.ClientModule != nullptr)
+	{
+		Win32App.bRunning = true;
+	}
+
+	MSG message;
+	while (Win32App.bRunning && GetMessage(&message, Win32App.MainWindow, NULL, NULL))
+	{
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	if (Win32App.bUsingConsole)
+	{
+		CloseConsole();
 	}
 
 	return 0;
